afa6b98751db0f045132f8424157cabadd84c08f
unknown <majikman552@gmail.com>
2026-01-05 20:43:48 +0530

    .

--- studydeck_forum/settings.py
+++ studydeck_forum/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 IS_RENDER = 'IS_RENDER' in os.environ
 
-DEBUG = not IS_RENDER
+DEBUG = IS_RENDER
 
 if IS_RENDER:
     ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
