fd762de416bf8c6146f52c9e3c9566dd161d8af7
unknown <majikman552@gmail.com>
2026-01-05 19:33:25 +0530

    trying to fix a db issue

--- studydeck_forum/settings.py
+++ studydeck_forum/settings.py
@@ -113,9 +113,8 @@ DATABASES = {
     }
 
 if IS_RENDER:
-    DATABASES = {
-        'default': dj_database_url.config(DATABASE_URL),
-    }
+    DATABASES['default'] = dj_database_url.parse(DATABASE_URL)
+    DATABASES['default']['OPTIONS'] = {'sslmode': 'require'}
 
     
 
