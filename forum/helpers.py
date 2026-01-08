5a42ddf77b671288f058cd9047cff9f107d35482
unknown <majikman552@gmail.com>
2026-01-08 23:38:08 +0530

    added back threading just so replies can actually work

--- forum/helpers.py
+++ forum/helpers.py
@@ -3,16 +3,19 @@ from django.conf import settings
 import threading
 
 def send_reply_notification(post, reply):
-    try:
-        subject = f"New reply on your post: {post.title}"
-        message = f"Hi {post.author.first_name},\n\n{reply.author} just replied to your post:\n\n'{reply.content[:100]}...'\n"
-        from_email = settings.EMAIL_HOST_USER
-        recipient_list = [post.author.email]
+    def send(post, reply):
+        try:
+            subject = f"New reply on your post: {post.title}"
+            message = f"Hi {post.author.first_name},\n\n{reply.author} just replied to your post:\n\n'{reply.content[:100]}...'\n"
+            from_email = settings.EMAIL_HOST_USER
+            recipient_list = [post.author.email]
 
-        # print(f"{post.title}")
-        
-        send_mail(subject, message, from_email, recipient_list)
-        print("SUCCESS YIPPEEE")
+            # print(f"{post.title}")
+            
+            send_mail(subject, message, from_email, recipient_list)
+            print("SUCCESS YIPPEEE")
 
-    except Exception as e:
-        print(f"Email Exception - {e}")
+        except Exception as e:
+            print(f"Email Exception - {e}")
+
+    threading.Thread(target=send, args=[post, reply]).start()
